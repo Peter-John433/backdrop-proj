9f97ab0a9728a9dc94550b2c811b0430cce8b748
iampeterjohn <peterjeche442@gmail.com>
2024-04-02 12:47:30 -1100

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-      hello world
+  <div class="cont">
+      <h1>
+        {{ title }}
+      </h1>
   </div>
+  <Modal />
 </template>
 
-<script setup>
+<script>
+ import Modal from './components/Modal.vue';
+export default{
+    name: 'App',
+    components: { Modal },
+    data(){
+      return{
+        title: 'Modal'
+      }
+    }
+}
 
 </script>
+
+<style scoped>
+   .cont{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 2rem auto;
+   }
+
+   h1{
+      color: blue;
+      font-size: 4rem;
+   }
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="backdrop">
+            <div class="modal">
+                <h1>{{ modalTitle }}</h1>
+                <div>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   export default{
+        data(){
+            return{
+                modalTitle: 'Modal'
+            }
+        }
+   }
+</script>
+
+<style scoped>
+  .backdrop{
+    position: fixed;
+    top: 0%;
+    background-color: rgba(0, 0, 0, .5);
+    width: 100%;
+    height: 100%;
+  }
+
+  .modal{
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     /* height: 100%; */
+     margin-top: 5rem;
+  }
+
+  .modal h1{
+     color: white;
+     font-size: 2.7rem;
+     text-transform: capitalize;
+  }
+
+  textarea{
+     width: 400px;
+     margin-top: 1rem;
+     border-radius: 16px;
+     resize: none;
+  }
+</style>
